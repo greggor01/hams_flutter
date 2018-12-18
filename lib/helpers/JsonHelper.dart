3dfa06e874d17ec63a4556db05c76c16d961adca
Greg Moore <gmoore@trilixtech.com>
2018-12-18 10:06:06 -0500

    use jsondecoder instead of jsoncodec

--- lib/helpers/JsonHelper.dart
+++ lib/helpers/JsonHelper.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 
 class JsonHelper {
   static List<dynamic> parseJSONArray(String json) {
-    JsonCodec codec = new JsonCodec();
-    var temp = codec.decode(json);
+    var temp = JsonDecoder().convert(json);
     return temp['result'];
   }
 }
